7dedbec3b6900779adf6087ee190d9acd28868a4
Raheel Ahmad <raheelA0@pm.me>
2022-07-27 19:10:20 -0400

    add book display functionality using input from modal form

--- library.js
+++ library.js
@@ -1,19 +1,32 @@
 let myLibrary = [];
 
 class Book {
-    constructor(title, author, numOfPages, readStatus) {
-        this.title = title;
-        this.author = author;
-        this.numOfPages = numOfPages;
-        this.readStatus = readStatus;
-    }
+  constructor (title, author, numOfPages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.readStatus = readStatus;
+  }
 }
 
-function addBookToLibrary(title, author, numOfPages, readStatus) {
-    const book1 = new Book(title, author, numOfPages, readStatus);
-    myLibrary.push(book1);
-    console.table(myLibrary);
+function addBookToLibrary (title, author, numOfPages, readStatus) {
+  const book1 = new Book(title, author, numOfPages, readStatus);
+  myLibrary.push(book1);
+  displayBook(book1);
 };
+
+function displayBook (book1) {
+
+  const bookCard = document.createElement('div');
+  bookCard.classList.add('bookCard');
+  for (key in book1){
+    const p = document.createElement("p");
+    p.innerHTML = book1[key];
+    bookCard.appendChild(p);
+  }
+  const books = document.getElementById("bookCards");
+  books.prepend(bookCard);
+}
 
 function removeBook() {
 
@@ -27,6 +40,8 @@ var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+
+var addbtn = document.getElementById("submit");
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -45,8 +60,18 @@ window.onclick = function(event) {
   }
 }
 
+addbtn.onclick = function() {
+  modal.style.display = "none";
+}
+
 document.querySelector('form').addEventListener('submit', (e) => {
-    const data = Object.fromEntries(new FormData(e.target).entries());
-    addBookToLibrary(data.title, data.author, data.numOfPages, 1);
-    document.getElementById("basic-form").reset();
-  });
+  const data = Object.fromEntries(new FormData(e.target).entries());
+  if (data.readStatus == "on"){
+    data.readStatus = 1;
+  } else {
+    data.readStatus = 0;
+  }
+  addBookToLibrary(data.title, data.author, data.numOfPages, data.readStatus);
+  document.getElementById("basic-form").reset();
+});
+
